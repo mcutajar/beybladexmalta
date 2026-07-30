e11b4896ffae663ae8bf5de46fb12ced6fe0d481
Matthew Cutajar <matthewcutajar83@gmail.com>
2026-07-30 20:25:07 +0200

    feat: declare strict types and enforce it with fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,12 +6,12 @@ $finder = (new PhpCsFixer\Finder())
     ->notPath([
         'config/bundles.php',
         'config/reference.php',
-    ])
-;
+    ]);
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
+        'declare_strict_types' => true,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
